ed1a3ffe66db17be3ffc043c2ca777b88e944636
Ben Mintz <bmintz@mit.edu>
2018-04-09 15:37:53 -0400

    change output to csv file

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,38 +1,38 @@
 import xlrd
+import csv
 from sys import argv
 
-def open_excel_file(filename):
-    '''
-    Takes excel workbook file name as argument
-    Opens excel workbook
-    opens spreadsheets
-    returns sheet object(s)
-    '''
-    workbook = xlrd.open_workbook(filename)
-    if workbook.nsheets > 1:
-        print("This Excel has too many spreadsheets")
-        print("Only 1 spreadsheet can be processed per file")
-    else:
-        calculate_points(workbook.sheet_by_index(0))
-        print(">>>> sheet_object: ", workbook.sheet_by_index(0))
+'''
+This is a proof-of-concept script to help with the final product
+The script was written for a specific Excel file in which 
+we already what type of data to expect in each of the columns
+(in this case, the 2018 NHL standings)
+In this example, it will take a spreadsheet with each NHL teams' 2018 win-loss record
+and will calculate the total points
+(In hockey 2 points for a win, 1 point for an overtime/shootout loss)
+'''
 
 def calculate_points(sheet_object):
-    '''
-    Takes a sheet object as an an argument.
-    Loops through each line of the sheet.
-    Extracts team name, wins and OTL and calculates points.
-    Note: This assumes we already know which columns we're
-    pulling from
-    '''
+    row_vals = []
     for i in range(1, sheet_object.nrows):
         points = (sheet_object.row_values(i)[1] * 2) + sheet_object.row_values(i)[3]
-        dump_to_text(sheet_object.row_values(i)[0], points)
+        row_vals.append([sheet_object.row_values(i)[0], int(points)])
+    dump_to_csv(row_vals)
 
-def dump_to_text(teamname, points):
-    results_file.write(f"{teamname}\t{points}\n")
+def dump_to_csv(vals):
+    with open('results.csv', "w") as file:
+        writer = csv.writer(file, delimiter='\t', lineterminator='\n')
+        writer.writerows(vals)
 
 if __name__ == '__main__':
-    _, excel_file, text_file = argv
-    results_file = open(text_file, "w")
-    open_excel_file(excel_file)
-    results_file.close()
+    '''
+    Takes Excel file name and sheet name as command line arguments
+    Checks if given sheet name exists in file. 
+    If the sheet doesn't exist, an error messager is displayed. 
+    '''
+    _, excel_file, sheet_name = argv
+    workbook = xlrd.open_workbook(excel_file)
+    if sheet_name not in workbook.sheet_names():
+        print(f"There is no sheet named {sheet_name} in this excel file")
+    else:
+        calculate_points(workbook.sheet_by_name(sheet_name))
