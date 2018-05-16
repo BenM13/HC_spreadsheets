50f705e9513ab6a98684c427b761a23e2d4986f7
Ben Mintz <bmintz@mit.edu>
2018-05-16 11:56:35 -0400

    revise readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # HC_spreadsheets
+
+This is a utility script for Her Campus staff. 
+It is designed to calulate the monthly points for team size,
+social circulation, and traffic circulation. 
+After calculating points, the script will write the totals
+to a .csv file.
--- monthly_points.py
+++ monthly_points.py
@@ -1,14 +1,6 @@
 import xlrd
 import csv
 
-"""
-This is a utility script for Her Campus staff. 
-It is designed to calulate the monthly points for team size,
-social circulation, and traffic circulation. 
-After calculating points, the script will write the totals
-to a .csv file
-"""
-
 
 def team_size_pts(size):
     """
